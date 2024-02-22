871b36e97ba0c4b710672e3eb72b2825ec0929b4
juliarenner <157319766+juliarenner@users.noreply.github.com>
2024-02-22 02:13:43 +0000

    Finished Lab 03

--- main.py
+++ main.py
@@ -4,65 +4,85 @@ See recitation-03.md for details.
 """
 import time
 
+
 class BinaryNumber:
-    """ done """
-    def __init__(self, n):
-        self.decimal_val = n               
-        self.binary_vec = list('{0:b}'.format(n)) 
-        
-    def __repr__(self):
-        return('decimal=%d binary=%s' % (self.decimal_val, ''.join(self.binary_vec)))
-    
+  """ done """
+
+  def __init__(self, n):
+    self.decimal_val = n
+    self.binary_vec = list('{0:b}'.format(n))
+
+  def __repr__(self):
+    return ('decimal=%d binary=%s' %
+            (self.decimal_val, ''.join(self.binary_vec)))
+
 
 ## Implement multiplication functions here. Note that you will have to
 ## ensure that x, y are appropriately sized binary vectors for a
 ## divide and conquer approach.
 
-def binary2int(binary_vec): 
-    if len(binary_vec) == 0:
-        return BinaryNumber(0)
-    return BinaryNumber(int(''.join(binary_vec), 2))
+
+def binary2int(binary_vec):
+  if len(binary_vec) == 0:
+    return BinaryNumber(0)
+  return BinaryNumber(int(''.join(binary_vec), 2))
+
 
 def split_number(vec):
-    return (binary2int(vec[:len(vec)//2]),
-            binary2int(vec[len(vec)//2:]))
+  return (binary2int(vec[:len(vec) // 2]), binary2int(vec[len(vec) // 2:]))
+
 
 def bit_shift(number, n):
-    # append n 0s to this number's binary string
-    return binary2int(number.binary_vec + ['0'] * n)
-    
-def pad(x,y):
-    # pad with leading 0 if x/y have different number of bits
-    # e.g., [1,0] vs [1]
-    if len(x) < len(y):
-        x = ['0'] * (len(y)-len(x)) + x
-    elif len(y) < len(x):
-        y = ['0'] * (len(x)-len(y)) + y
-    # pad with leading 0 if not even number of bits
-    if len(x) % 2 != 0:
-        x = ['0'] + x
-        y = ['0'] + y
-    return x,y
+  # append n 0s to this number's binary string
+  return binary2int(number.binary_vec + ['0'] * n)
+
+
+def pad(x, y):
+  # pad with leading 0 if x/y have different number of bits
+  # e.g., [1,0] vs [1]
+  if len(x) < len(y):
+    x = ['0'] * (len(y) - len(x)) + x
+  elif len(y) < len(x):
+    y = ['0'] * (len(x) - len(y)) + y
+  # pad with leading 0 if not even number of bits
+  if len(x) % 2 != 0:
+    x = ['0'] + x
+    y = ['0'] + y
+  return x, y
+
 
 def quadratic_multiply(x, y):
-    # this just converts the result from a BinaryNumber to a regular int
-    return _quadratic_multiply(x,y).decimal_val
+  # this just converts the result from a BinaryNumber to a regular int
+  return _quadratic_multiply(x, y).decimal_val
+
 
 def _quadratic_multiply(x, y):
-    ### TODO
-    pass
-    ###
+  xvec, yvec = pad(x.binary_vec, y.binary_vec)
+
+  # base case
+  if binary2int(xvec).decimal_val <= 1 and binary2int(yvec).decimal_val <= 1:
+    return BinaryNumber(
+        binary2int(xvec).decimal_val * binary2int(yvec).decimal_val)
+
+  x_left, x_right = split_number(xvec)
+  y_left, y_right = split_number(yvec)
+
+  l_halves = bit_shift(_quadratic_multiply(x_left, y_left), len(xvec))
+  cross_sum_1 = bit_shift(_quadratic_multiply(x_left, y_right), len(xvec) // 2)
+  cross_sum_2 = bit_shift(_quadratic_multiply(x_right, y_left), len(xvec) // 2)
+  r_halves = _quadratic_multiply(x_right, y_right)
+
+  return BinaryNumber(l_halves.decimal_val + cross_sum_1.decimal_val +
+                      cross_sum_2.decimal_val + r_halves.decimal_val)
 
 
-    
-    
-def test_quadratic_multiply(x, y, f):
-    start = time.time()
-    # multiply two numbers x, y using function f
-    
-    return (time.time() - start)*1000
+def test_quadratic_multiply():
+  start = time.time()
+  # multiply two numbers x, y using function f
+  print(_quadratic_multiply(BinaryNumber(10), BinaryNumber(5)))
+  print(_quadratic_multiply(BinaryNumber(7), BinaryNumber(3)))
+  runtime = (time.time() - start) * 1000
+  print(runtime, "milliseconds")
 
 
-    
-    
-
+print(test_quadratic_multiply())
--- test_main.py
+++ test_main.py
@@ -1,7 +1,12 @@
 from main import *
 
 
-
 ## Feel free to add your own tests here.
 def test_multiply():
-    assert quadratic_multiply(BinaryNumber(3), BinaryNumber(3)) == 3*3
+  assert quadratic_multiply(BinaryNumber(3), BinaryNumber(3)) == 3 * 3
+  assert quadratic_multiply(BinaryNumber(5), BinaryNumber(6)) == 5 * 6
+  assert quadratic_multiply(BinaryNumber(12), BinaryNumber(1)) == 12 * 1
+  assert quadratic_multiply(BinaryNumber(150), BinaryNumber(0)) == 150 * 0
+
+
+print(test_multiply())
